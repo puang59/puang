c5c4069f61ebc93b2d54ecabc227d3cec789536e
puang59 <kk61468@gmail.com>
2025-04-07 00:04:52 +0530

    fix for vercel

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
+      },
+      colors: {
+        background: "#000000",
+        foreground: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
